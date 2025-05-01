085dd3cbc81cfb6d0f84bf6558191b98e57be6e3
vinnyaquino <viniciusaquino123@gmail.com>
2025-05-01 05:52:41 -0300

    feat: adição do widget de ferramentas ao header

--- src/layouts/header-menu-right.tsx
+++ src/layouts/header-menu-right.tsx
@@ -1,19 +1,21 @@
-import { Badge } from "rizzui/badge";
-import { ActionIcon } from "rizzui/action-icon";
-import MessagesDropdown from "@/layouts/messages-dropdown";
-import ProfileMenu from "@/layouts/profile-menu";
-import RingBellSolidIcon from "@core/components/icons/ring-bell-solid";
-import ChatSolidIcon from "@core/components/icons/chat-solid";
-import NotificationDropdown from "./notification-dropdown";
+'use client';
+
+import { Badge, ActionIcon } from 'rizzui';
+import MessagesDropdown from '@/layouts/messages-dropdown';
+import ProfileMenu from '@/layouts/profile-menu';
+import SettingsButton from '@/layouts/settings-button';
+import RingBellSolidIcon from '@core/components/icons/ring-bell-solid';
+import ChatSolidIcon from '@core/components/icons/chat-solid';
+import NotificationDropdown from './notification-dropdown';
 
 export default function HeaderMenuRight() {
   return (
-    <div className="ms-auto grid shrink-0 grid-cols-3 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
+    <div className="ms-auto grid shrink-0 grid-cols-4 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
       <NotificationDropdown>
         <ActionIcon
           aria-label="Notification"
           variant="text"
-          className="relative h-[34px] w-[34px] shadow backdrop-blur-md md:h-9 md:w-9 dark:bg-gray-100"
+          className="relative h-[34px] w-[34px] shadow backdrop-blur-md dark:bg-gray-100 md:h-9 md:w-9"
         >
           <RingBellSolidIcon className="h-[18px] w-auto" />
           <Badge
@@ -40,6 +42,7 @@ export default function HeaderMenuRight() {
         </ActionIcon>
       </MessagesDropdown>
 
+      <SettingsButton />
       <ProfileMenu />
     </div>
   );
